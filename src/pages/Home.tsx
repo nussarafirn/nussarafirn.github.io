5e06f5f553830aeb96e6dcf9a4d0b5050446f259
Nussara 'Firn' Tieanklin <nussara@cs.washington.edu>
2022-10-23 14:37:20 -0700

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,24 @@ import React from 'react';
 function home() {
   return (
     <>
-      <img src="img/nussara_headshot.jpg" className='img-mask' width={300}></img>
-      <p>Hello TESTING</p>
+      <img src="img/nussara_headshot.jpg" className='img-mask' width={300} alt="my headshot"></img>
+      <p>
+        Hello! My name is Nussara Tieanklin or known among friends and family as Firn.
+        I am a second-year Ph.D. Student advised by Professor Kurtis Heimerl in Information
+        and Communication Technology for Development (ICTD) Lab at Paul G. Allen School,
+        University of Washington (Seattle, WA).
+      </p>
+      <p>
+        My research interests lie within the umbrella of Data Science for social goods using
+        mixed methods research and various machine learning approaches.
+        The current research in computing for Social Good is included but not limited to analyzing
+        the effects of air pollution on the motorcycle-taxi Grab workers in Southeast Asia. 
+      </p>
+      <p>
+        I am also interested in using community networks bridging the digital divide.
+        As a techno-social lead in the Seattle Community Network, I conduct multiple qualitative researches,
+        help with installations, teach technical workshops, and expand community outreach. 
+      </p>
     </>
 
   );
