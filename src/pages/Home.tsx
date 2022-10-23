b36fcef70e3de6ccd786e493e81ab76cc8e18c53
Nussara 'Firn' Tieanklin <nussara@cs.washington.edu>
2022-10-23 14:31:17 -0700

    remove paralax

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
+// import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 
 function home() {
   return (
