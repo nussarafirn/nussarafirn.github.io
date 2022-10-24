fbb6076e6083193aedb4d6401f7dd9a87b5dae2a
chanwutk <30903997+chanwutk@users.noreply.github.com>
2022-10-24 12:52:17 +0000

    Deploying to gh-pages from @ nussarafirn/nussarafirn.github.io@3cf5e0a5e058204d3e99b8b69a8deeecb401416a 🚀

--- _next/static/chunks/pages/pages/App-fd34602a37c4c81a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[655],{6539:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/pages/App",function(){return u(4010)}])}},function(n){n.O(0,[634,10,774,888,179],(function(){return _=6539,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
