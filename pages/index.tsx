5d47cff8ff0500d366b641b421b4154a8b31f590
Chanwut (Mick) Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2022-10-23 18:07:18 -0700

    use next js (#2)
    * use next
    
    * deploy
    
    * package-lock
    
    * .
    
    * web-vitals
    
    * migrate
    
    * ci

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+const Home: NextPage = () => {
+  return (
+    <App />
+  )
+}
+
+reportWebVitals();
+
+export default Home
