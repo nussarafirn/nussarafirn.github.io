dabdd337e05ae98e51141b4a6e0c52c86549773e
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-11-07 22:48:14 -0800

    use gdrive pdf viewer

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -4,12 +4,13 @@ import reportWebVitals from './reportWebVitals'
 
 const Home: NextPage = () => {
   return <CV title="CV | Nussara 'Firn' Tieanklin">
-    <embed
-      src="https://nussarafirn.github.io/resume/nussara.pdf"
-      type="application/pdf"
-      width="100%"
+    <iframe
+      src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://nussarafirn.github.io/resume/resume.pdf#toolbar=0&scrollbar=0"
+      frameBorder="0"
+      scrolling="auto"
       height="1000px"
-    />
+      width="100%"
+    ></iframe>
   </CV>
 }
 
