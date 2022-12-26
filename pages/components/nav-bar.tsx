97894fed5cc470dabc6e45dcebde92a97f6f7c37
Chanwut (Mick) Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2022-12-26 04:39:29 -0800

    Update nav-bar.tsx

--- pages/components/nav-bar.tsx
+++ pages/components/nav-bar.tsx
@@ -6,11 +6,11 @@ function NavBar() {
   return <div className="nav-bar"> 
     <Firn/>
     <div className="nav-menu">
-      {/* <NavButton href="./work">Work</NavButton>
-      <NavButton href="./pub">Publications</NavButton>
-      <NavButton href="./blog">Blog</NavButton> */}
+      {/* <NavButton href="./work.html">Work</NavButton>
+      <NavButton href="./pub.html">Publications</NavButton>
+      <NavButton href="./blog.html">Blog</NavButton> */}
       {/* <NavButton href="https://nussarafirn.github.io/resume/">CV</NavButton> */}
-      <NavButton href="./cv">CV</NavButton>
+      <NavButton href="./cv.html">CV</NavButton>
     </div>
   </div>;
 }
